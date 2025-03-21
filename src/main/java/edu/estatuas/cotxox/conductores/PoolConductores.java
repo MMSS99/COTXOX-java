fab132ed29fe76b9c61048602ced050b460d9d52
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-21 13:56:19 +0100

    feat(conductor): :sparkles: add asignarConductor random loop

--- src/main/java/edu/estatuas/cotxox/conductores/PoolConductores.java
+++ src/main/java/edu/estatuas/cotxox/conductores/PoolConductores.java
@@ -1,4 +1,37 @@
 package edu.estatuas.cotxox.conductores;
 
+import java.util.List;
+import java.util.Random;
+
 public class PoolConductores {
+    private List<Conductor> listaConductores;
+
+    public PoolConductores(List<Conductor> listaConductores) {
+        this.listaConductores = listaConductores;
+    }
+
+    public List<Conductor> getListaConductores() {
+        return this.listaConductores;
+    }
+
+    public Conductor asignarConductor(){
+        Conductor conductorSeleccionado = null;
+        Random random = new Random();
+        int intentofallido = 0;
+        final int MAXINTENTOS = 10;
+        boolean asignado = false;
+
+
+        while (intentofallido < MAXINTENTOS && !asignado) {
+            conductorSeleccionado = this.listaConductores.get(random.nextInt(this.listaConductores.size()));
+            if (conductorSeleccionado.isOcupado()){
+                intentofallido++;
+        } else {
+                conductorSeleccionado.setOcupado(true);
+                asignado = true;
+            }
+
+        }
+        return conductorSeleccionado;
+    }
 }
